2feb04c1ff2585adda29ddb34751436782ec7afa
Edil Biktimirov <idyabik@gmail.com>
2022-10-25 14:18:49 +0600

    Добавил css стили в App.css
    Отредактировал код в App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import CardView from "./CardView/CardView";
 import CardDeck from "./lib/CardDeck";
 import Card from "./lib/Card";
 import PokerHand from "./lib/PokerHand";
+import './App.css'
 
 let handResult = '';
 
@@ -16,28 +17,28 @@ function App() {
 
     const Hand = new PokerHand(newCards);
     handResult = Hand.getOutcome();
-  }
+  };
 
-  if (cardsStatus.length === 0) {
-    return (
-      <div className="App">
-        <button onClick={dealCards}>Раздать карты</button>
-      </div>);
-  } else {
-    return (
-      <div className="App">
-        <button onClick={dealCards}>Раздать карты</button>
-        <div className="playingCards faceImages">
-          <CardView rank={cardsStatus[0].rank} suit={cardsStatus[0].suit}/>
-          <CardView rank={cardsStatus[1].rank} suit={cardsStatus[1].suit}/>
-          <CardView rank={cardsStatus[2].rank} suit={cardsStatus[2].suit}/>
-          <CardView rank={cardsStatus[3].rank} suit={cardsStatus[3].suit}/>
-          <CardView rank={cardsStatus[4].rank} suit={cardsStatus[4].suit}/>
+    if (cardsStatus.length === 0) {
+      return (
+        <div className="App">
+          <button onClick={dealCards} className="mainBtn">Раздать карты</button>
+        </div>);
+    } else {
+      return (
+        <div className="App">
+          <button onClick={dealCards} className="mainBtn">Раздать карты</button>
+          <div className="playingCards faceImages cardsDiv">
+            <CardView rank={cardsStatus[0].rank} suit={cardsStatus[0].suit}/>
+            <CardView rank={cardsStatus[1].rank} suit={cardsStatus[1].suit}/>
+            <CardView rank={cardsStatus[2].rank} suit={cardsStatus[2].suit}/>
+            <CardView rank={cardsStatus[3].rank} suit={cardsStatus[3].suit}/>
+            <CardView rank={cardsStatus[4].rank} suit={cardsStatus[4].suit}/>
+          </div>
+          <p className="result">{handResult}</p>
         </div>
-        <p>{handResult}</p>
-      </div>
-    );
+      );
+    }
   }
-}
 
 export default App;
